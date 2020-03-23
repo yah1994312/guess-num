d7d1842537709281a1ad6341c86feaf20c327835
Gordon Chang <496929145@qq.com>
2020-03-23 18:06:02 +0800

    first file

---
+++ guess-num.py
@@ -0,0 +1,13 @@
+import random
+
+r = random.randint(1 , 100)
+while True:
+	num = input('猜個數字：')
+	num = int(num)
+	if num == r:
+		print('終於猜對了！')
+		break
+	elif num > r:
+		print('比答案大')
+	else:
+		print('比答案小')
